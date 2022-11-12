628fb80a9c6a2794b9cb31b54cfd1d00ee5235a5
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2022-11-12 21:54:19 +0300

    @ModelAttribute

--- src/main/java/ru/philimonov/spring/controllers/PeopleController.java
+++ src/main/java/ru/philimonov/spring/controllers/PeopleController.java
@@ -4,9 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import ru.philimonov.spring.dao.PersonDAO;
+import ru.philimonov.spring.models.Person;
 
 @Controller
 @RequestMapping("/people")
@@ -29,5 +32,16 @@ public class PeopleController {
     public String show(@PathVariable("id") int id, Model model) {
         model.addAttribute("person", personDAO.show(id));
         return "people/show";
+    }
+
+    @GetMapping("/new")
+    public String newPerson(@ModelAttribute("person") Person person){
+        return "people/new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("person") Person person){
+        personDAO.save(person);
+        return "redirect:/people";
     }
 }
