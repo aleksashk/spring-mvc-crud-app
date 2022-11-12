6b22b429c2734203c28136594940174d7497cdd8
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2022-11-12 23:20:38 +0300

    DELETE request

--- src/main/java/ru/philimonov/spring/controllers/PeopleController.java
+++ src/main/java/ru/philimonov/spring/controllers/PeopleController.java
@@ -3,6 +3,7 @@ package ru.philimonov.spring.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -57,4 +58,11 @@ public class PeopleController {
         personDAO.update(id, person);
         return "redirect:/people";
     }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") int id){
+        personDAO.delete(id);
+        return "redirect:/people";
+    }
+
 }
--- src/main/java/ru/philimonov/spring/dao/PersonDAO.java
+++ src/main/java/ru/philimonov/spring/dao/PersonDAO.java
@@ -38,4 +38,8 @@ public class PersonDAO {
 
         personToBeUpdated.setName(updatePerson.getName());
     }
+
+    public void delete(int id) {
+        people.removeIf(person -> person.getId() == id);
+    }
 }
