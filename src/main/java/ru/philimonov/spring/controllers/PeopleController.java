b9fccbd6b578368e5c2f4d2694a48cbe1009de92
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2022-11-13 12:47:13 +0300

    Hibernate Validator

--- src/main/java/ru/philimonov/spring/controllers/PeopleController.java
+++ src/main/java/ru/philimonov/spring/controllers/PeopleController.java
@@ -1,8 +1,10 @@
 package ru.philimonov.spring.controllers;
 
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -42,7 +44,12 @@ public class PeopleController {
     }
 
     @PostMapping()
-    public String create(@ModelAttribute("person") Person person) {
+    public String create(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            return "people/new";
+        }
+
         personDAO.save(person);
         return "redirect:/people";
     }
@@ -54,13 +61,17 @@ public class PeopleController {
     }
 
     @PatchMapping("/{id}")
-    public String update(@ModelAttribute("person") Person person, @PathVariable("id") int id){
+    public String update(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult, @PathVariable("id") int id) {
+
+        if (bindingResult.hasErrors()) {
+            return "people/edit";
+        }
         personDAO.update(id, person);
         return "redirect:/people";
     }
 
     @DeleteMapping("/{id}")
-    public String delete(@PathVariable("id") int id){
+    public String delete(@PathVariable("id") int id) {
         personDAO.delete(id);
         return "redirect:/people";
     }
